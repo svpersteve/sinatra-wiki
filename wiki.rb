b2efb29c2711a35c270fcf6f45b7e9035dee477f
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-17 12:29:20 +0100

    Users can create new pages

--- wiki.rb
+++ wiki.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'uri'
 
 def page_content(title)
   File.read("pages/#{title}.txt")
@@ -6,9 +7,24 @@ rescue Errno::ENOENT
   return 'Not found'
 end
 
+def save_content(title, content)
+  File.open("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+  end
+end
+
 get '/' do
   'Hi'
   erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/create' do
+  save_content(params[:title], params[:content])
+  redirect URI.escape "/#{params[:title]}"
 end
 
 get '/:title' do
