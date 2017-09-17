f3f486bda1b2b1e8c66b04ffd9c2a3ec24f0cf4f
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-17 12:28:15 +0100

    Page show route and view

--- wiki.rb
+++ wiki.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return 'Not found'
+end
+
 get '/' do
   'Hi'
   erb :index
+end
+
+get '/:title' do
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :show
 end
 end
